1e9716891ef1559f32e94516f960479d1956be2a
elijhonathan <eligrancej@hotmail.com>
2024-02-22 17:17:09 -0300

    ok

---
+++ 03-AulaC-WhatsYourName.c
@@ -0,0 +1,11 @@
+int main(int argc, char const *argv[])
+{
+
+    char nome[100];
+
+    printf("Qual o seu nome: ");
+    scanf("%s", nome);
+
+    printf("Seu nome eh %s", nome);
+    return 0;
+}
