d783a9c8d1dfd4a3cfc252c07c82788cd0038246
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2024-03-02 21:22:46 -0300

    Create 04-AulaOperadores.c

---
+++ 04-AulaOperadores.c
@@ -0,0 +1,16 @@
+int main(void)
+{
+
+    int x, y;
+
+    printf("Digite o X: ");
+    scanf("%i", &x);
+
+    printf("Digite o Y: ");
+    scanf("%i", &y);    
+
+    float z = (float) x / (float) y;
+
+    printf("%f", z);
+
+}
